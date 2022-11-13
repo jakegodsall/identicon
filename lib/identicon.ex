f950430715ef4487237a0294e2160778064ec15d
jakegodsall <jake.edward.godsall@gmail.com>
2022-11-13 10:13:08 +0000

    added doctest for Identicon.mirror_row

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,8 +3,12 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_colour
+    |> build_grid
   end
 
+  @doc """
+    Takes string input and returns `Identicon.Image{hex: hex}` struct where `hex` is the hexadecimal representation of the MD5 hash of the string.
+  """
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
@@ -16,6 +20,23 @@ defmodule Identicon do
     %Identicon.Image{image | colour: {red, green, blue}}
   end
 
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(&mirror_row/1)
+  end
 
+  @doc """
+    Takes an three element list and returns a five element symmetric list.any()
 
+  ## Examples
+
+        iex> Identicon.mirror_row([1, 2, 3])
+        [1, 2, 3, 2, 1]
+
+  """
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
 end
